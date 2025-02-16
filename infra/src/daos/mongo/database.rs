0c941c1441f5f973b9e8ed8cdd31e6b3ef9a0922
paq1 <p.paquin1605@gmail.com>
2025-02-16 16:59:52 +0100

    chore: change error message

--- infra/src/daos/mongo/database.rs
+++ infra/src/daos/mongo/database.rs
@@ -8,7 +8,7 @@ pub struct DatabaseMongo {
 impl DatabaseMongo {
     pub async fn new(uri: &str, dbname: &str) -> ResultErr<Self> {
         let client = Client::with_uri_str(uri).await.map_err(|err| {
-            ErrorWithCodeBuilder::new("00MGCONCLI", 500, "connection au client mongo en erreur")
+            ErrorWithCodeBuilder::new("00MGCONCLI", 500, "Erreur lors de la connection au client mongo.")
                 .with_problems(vec![Problem {
                     title: format!("mongo error : {err}"),
                     description: None,
